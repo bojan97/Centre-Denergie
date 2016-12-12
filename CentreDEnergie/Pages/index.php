c3ba8f138f7d63e9f30470caf13bc27bdb31b014
bojan97 <b2thes@hotmail.com>
2016-12-12 11:41:01 -0500

    Add files via upload

---
+++ CentreDEnergie/Pages/index.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Centre d'Énergie de Chambly'</title>
+	<link rel="stylesheet" href="/Content/bootstrap.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+			<div class="col-md-1"></div>
+				<div class="col-md-9">
+				<img src="/CentreDEnergie/Content/logo.png">
+					<h4>
+						Centre d'Énergie de Chambly
+					</h4>
+				</div>
+				<div class="col-md-2">
+					 <a class="label label-default">Enregistrer</a><a class="label label-default">Connexion</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-5">
+					<ul class="nav nav-pills">
+						<li class="active">
+							<a href="/CentreDEnergie/Pages/acceuil.php">Acceuil</a>
+						</li>
+						<li>
+							<a href="/CentreDEnergie/Pages/styles.php">Styles de Combat</a>
+						</li>
+						<li >
+							<a href="/CentreDEnergie/Pages/cours.php">Le Cours</a>
+						</li>
+						<li>
+							<a href="/CentreDEnergie/Pages/salle.php">Salle de Discussion</a>
+						</li>
+						<li >
+							<a href="/CentreDEnergie/Pages/contact.php">Contactez-Nous</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>	
