dd26d0e7d8f27d82698521e45eb52e9c4684ef2d
bojan97 <b2thes@hotmail.com>
2016-12-12 13:02:25 -0500

    Add files via upload

--- CentreDEnergie/Pages/index.php
+++ CentreDEnergie/Pages/index.php
@@ -10,7 +10,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Centre d'Énergie de Chambly'</title>
-	<link rel="stylesheet" href="/Content/bootstrap.css">
+	<link rel="stylesheet" href="/CentreDEnergie/Content/bootstrap.css">
+	<link rel="stylesheet" href="/CentreDEnergie/Content/layoutCSS.css">
 </head>
 <body>
 <div class="container-fluid">
@@ -31,10 +32,10 @@
 			<div class="row">
 				<div class="col-md-4">
 				</div>
-				<div class="col-md-5">
+				<nav class="col-md-5">
 					<ul class="nav nav-pills">
 						<li class="active">
-							<a href="/CentreDEnergie/Pages/acceuil.php">Acceuil</a>
+							<a href="/CentreDEnergie/Pages/index.php">Acceuil</a>
 						</li>
 						<li>
 							<a href="/CentreDEnergie/Pages/styles.php">Styles de Combat</a>
@@ -49,12 +50,37 @@
 							<a href="/CentreDEnergie/Pages/contact.php">Contactez-Nous</a>
 						</li>
 					</ul>
-				</div>
+				</nav>
 				<div class="col-md-3">
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
+				<!-- Insert main-->
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+				<hr>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-4" style="text-align:center">
+					<hr>
+					<a href="/CentreDEnergie/Pages/index.php">Acceuil</a>&nbsp;|&nbsp;<a href="/CentreDEnergie/Pages/contact.php">Contactez-Nous</a>&nbsp;|&nbsp;<a href="https://www.facebook.com/Le-Centre-d%C3%89nergie-de-Chambly-56111876820/">Facebook</a>
+					<hr>
+					<br><br>
+					<footer><small>(514)231-5222</small>
+					<br>
+					<small>250 boulevard Fréchette, J3L 2Z5 Chambly, QC, Canada</small>
+					<br>
+					<small>test@hotmail.com</small><br>
+					<h6>Site créé par Bojan Srbinoski et Saad Ahmed</h6>
+					</footer>
+				</div>
+				<div class="col-md-4">
 				</div>
 			</div>
 		</div>
