847c87a063fd802c207f137bcf6f854d5ca54811
bojan97 <b2thes@hotmail.com>
2016-12-14 12:59:08 -0500

    wednesday commit
    -slider added
    -layout page separated
    -logic for active nav tab added
    -posts started

---
+++ CentreDEnergie/Pages/headerLayout.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Centre d'Énergie de Chambly'</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/CentreDEnergie/Content/layoutCSS.css">
+<?php
+	$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	$currentPage="index";
+	if (strpos($url,'index') !== false||strcmp($_SERVER['REQUEST_URI'],'/CentreDEnergie/Pages/')==0) 
+	{
+		echo "<link rel='stylesheet' href='/CentreDEnergie/Content/indexCSS.css'>";
+		$currentPage="index";
+	} 
+	else if(strpos($url,'styles') !== false)
+	{
+		$currentPage="styles";
+	}
+	else if(strpos($url,'cours') !== false)
+	{
+		$currentPage="cours";
+	}
+	else if(strpos($url,'salle') !== false)
+	{
+		$currentPage="salle";
+	}
+	else if(strpos($url,'contact') !== false)
+	{
+		$currentPage="contact";
+	}
+	
+	
+	
+	
+?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-md-3"></div>
+		<div class="col-md-7" id="banner">
+	
+		</div>
+		<div class="col-md-2" id="login-links">
+			 <a>Enregistrer</a>&nbsp;&nbsp;<a>Connexion</a>
+		</div>
+	</div>
+	<div class="row">
+		<nav class="col-md-12" >
+			<ul class="nav nav-pills">
+				<li <?php if(strcmp($currentPage,"index")==0){echo " class='active'";}?>>
+					<a href="/CentreDEnergie/Pages/index.php">Acceuil</a>
+				</li>
+				<li <?php if(strcmp($currentPage,"styles")==0){echo " class='active'";}?>>
+					<a href="/CentreDEnergie/Pages/styles.php">Styles de Combat</a>
+				</li>
+				<li <?php if(strcmp($currentPage,"cours")==0){echo " class='active'";}?>>
+					<a href="/CentreDEnergie/Pages/cours.php">Le Cours</a>
+				</li>
+				<li <?php if(strcmp($currentPage,"salle")==0){echo " class='active'";}?>>
+					<a href="/CentreDEnergie/Pages/salle.php">Salle de Discussion</a>
+				</li>
+				<li <?php if(strcmp($currentPage,"contact")==0){echo " class='active'";}?>>
+					<a href="/CentreDEnergie/Pages/contact.php">Contactez-Nous</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- CentreDEnergie/Pages/index.php
+++ CentreDEnergie/Pages/index.php
@@ -1,99 +1,75 @@
 <?php
+include("headerLayout.php");
 
-
-
-
+//echo "<a class='".$class."'></a>"
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Centre d'Énergie de Chambly'</title>
-	<link rel="stylesheet" href="/CentreDEnergie/Content/bootstrap.css">
-	<link rel="stylesheet" href="/CentreDEnergie/Content/layoutCSS.css">
-
-</head>
-<body>
-<div class="container-fluid">
-	<div class="row">
-	<div class="col-md-3"></div>
-		<div class="col-md-7" id="banner">
-		<!--<img src="/CentreDEnergie/Content/logotest1.jpg">-->
-	
-		</div>
-		<div class="col-md-2" id="login-links">
-			 <a>Enregistrer</a>&nbsp;&nbsp;<a>Connexion</a>
-		</div>
-	</div>
-	<div class="row">
-		<nav class="col-md-12" >
-			<ul class="nav nav-pills">
-				<li class="active">
-					<a href="/CentreDEnergie/Pages/index.php">Acceuil</a>
-				</li>
-				<li>
-					<a href="/CentreDEnergie/Pages/styles.php">Styles de Combat</a>
-				</li>
-				<li >
-					<a href="/CentreDEnergie/Pages/cours.php">Le Cours</a>
-				</li>
-				<li>
-					<a href="/CentreDEnergie/Pages/salle.php">Salle de Discussion</a>
-				</li>
-				<li >
-					<a href="/CentreDEnergie/Pages/contact.php">Contactez-Nous</a>
-				</li>
-			</ul>
-		</nav>
-	</div>
-	<!--End of Header-->
 	
 	<div class="row">
 		<div class="col-md-12">
 			<!-- Insert main-->
 			<div id="main">
-				<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-			</div>
-		</div>
-	</div>
-	
-	<!--Beginning of Footer-->
-	<div class="row">
-		<div class="col-md-3">
-		</div>	
-		<div class="col-md-6">
-			<hr>
-		</div>	
-		<div class="col-md-3">
-		</div>	
-	</div>
-		
-	<footer>
-		
-	<div class="row">
-		<div class="col-md-4">
-		</div>
-		<div class="col-md-4"  id="footer">
-			<hr>
-			<div id="bottom-links">
-				<a href="/CentreDEnergie/Pages/index.php">Acceuil</a>&nbsp;|&nbsp;<a href="/CentreDEnergie/Pages/contact.php">Contactez-Nous</a>&nbsp;|&nbsp;<a href="https://www.facebook.com/Le-Centre-d%C3%89nergie-de-Chambly-56111876820/">Facebook</a>
-			</div>
-			<hr>
-			<br>
-			<div id="contact-bottom">
-				<small>(514)231-5222</small>
-				<br>
-				<small>250 boulevard Fréchette, J3L 2Z5 Chambly, QC, Canada</small>
-				<br>
-				<small>test@hotmail.com</small>
-				<h6>Site créé par Bojan Srbinoski et Saad Ahmed</h6>
-			</div>
-		</div>
-		<div class="col-md-4">
-		</div>
-	</div>
-	</footer>
+				
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active" >
+      <img class="slideshow-images"src="/CentreDEnergie/Content/img1.png" >
+    </div>
+
+    <div class="item">
+      <img class="slideshow-images"src="/CentreDEnergie/Content/img2.png">
+    </div>
+
+    <div class="item">
+      <img class="slideshow-images"src="/CentreDEnergie/Content/img3.png" >
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
-</body>
-</html>	
+</div>
+</div>
+</div>
+
+<div class="post">
+	<h3>Bienvenue!</h3>
+	<img>
+	<br>
+	<p class="postContent">Bienvenue sur mon site! Vous pouvez naviguer à travers les pages de styles de combat ou du cours pour plus d'informations sur le programme offert. 
+		Vous pouvez aussi
+		créer un compte gratuitement et utiliser la salle de dicussion pour des questions reliées au matériel du cours. De plus, vous pouvez consultez votre profil 
+		pour une liste de tous
+		les techniques nécessaires pour votre prochain examen. Si vous avez des questions, veuillez consulter la page "Contactez-Nous".
+	</p>
+</div>
+	
+	
+	
+	
+
+	
+	
+	
+<?php
+
+
+include("footerLayout.php")
+
+
+?>
