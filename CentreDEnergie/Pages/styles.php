ade7f0df5c2a83747546d619922c92dc90fca262
bojan97 <b2thes@hotmail.com>
2016-12-15 11:13:56 -0500

    Add files via upload

--- CentreDEnergie/Pages/styles.php
+++ CentreDEnergie/Pages/styles.php
@@ -4,60 +4,7 @@ include("headerLayout.php");
 
 
 ?>
-<div class="row">
-	<div class="col-md-12">
-		<!-- Insert main-->
-		<div id="main">
-			<div id="tiger">
-				<img src="/CentreDEnergie/Content/tiger.jpg" alt="Tigre" id="tiger_img" width="640" height="426">
-				<h3>Tigre</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae cursus nibh. Donec sit amet nisl ut nisi pretium dictum. Phasellus pharetra magna in nisl accumsan dapibus et eget quam. 
-					Maecenas elementum faucibus ornare. Proin non scelerisque mi. Aliquam ornare scelerisque erat, ac sodales erat vehicula eget. Fusce in vari
-				</p>
-			</div>
-			
-			<div id="monkey">
-				<img src="/CentreDEnergie/Content/monkey.jpg" alt="Singe" id="monkey_img" width="640" height="426">
-				<h3>Singe</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae cursus nibh. 
-				</p>
-			</div>
-			
-			<div id="snake">
-				<img src="/CentreDEnergie/Content/snake.jpg" alt="Serpent" id="snake_img" width="640" height="426">
-				<h3>Serpent</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae cursus nibh. Donec sit amet nisl ut nisi pretium dictum. Phasellus pharetra magna in nisl accumsan dapibus et eget quam. 
-					Maecenas elementum faucibus ornare. Proin non scelerisque mi.  
-				</p>
-			</div>
-			
-			<div id="crane">
-				<img src="/CentreDEnergie/Content/crane.jpg" alt="Grue" id="crane_img" width="640" height="426">
-				<h3>Grue</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae cursus nibh. Donec sit amet nisl ut nisi pretium dictum. Phasellus pharetra magna in nisl accumsan dapibus et eget quam. 
-					Maecenas elementum faucibus ornare. Proin non scelerisque mi. Aliquam ornare scelerisque erat, ac sodales erat vehicula eget. Fusce in varius velit. Donec consequat dolor quis dui porttitor hendrerit. 
-					Donec mattis nisl quis tortor mattis condimentum. Nam eu posuere ante, ac ultrices libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae cursus nibh. Donec sit amet nisl ut nisi pretium dictum. Phasellus pharetra magna in nisl accumsan dapibus et eget quam. 
-					Maecenas elementum faucibus ornare. Proin non scelerisque mi. Aliquam ornare scelerisque erat, ac sodales erat vehicula eget. Fusce in varius velit. Donec consequat dolor quis dui porttitor hendrerit. 
-					Donec mattis nisl quis tortor mattis condimentum. Nam eu posuere ante, ac ultrices libero. 
-				</p>
-			</div>
-			
-			<div id="leopard">
-				<img src="/CentreDEnergie/Content/leopard.jpg" alt="Leopard" id="leopard_img" width="640" height="426">
-				<h3>Leopard</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae cursus nibh. Donec sit amet nisl ut nisi pretium dictum. Phasellus pharetra magna in nisl accumsan dapibus et eget quam. 
-					Maecenas elementum faucibus ornare. Proin non scelerisque mi. Aliquam ornare scelerisque erat, ac sodales erat vehicula eget. 
-				</p>
-			</div>
-			
-		</div>
-	</div>
-</div>
+
 
 
 
