1fd56db66e3e71bdc72b092d60bc0daeebc23e29
zaad97 <saad_ahmed1997@hotmail.com>
2017-01-12 16:02:22 -0500

    Add files via upload

---
+++ CentreDEnergie/Pages/mobileTest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name= "viewport" content="width=device-width,initial-scale=1.0">
+	<title>Centre d'Énergie de Chambly'</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/CentreDEnergie/Content/testCSS.css">
+	<link rel="icon" href="/CentreDEnergie/Content/favicon.ico" />
+	
+</head>
+<body>
+	<p>TESTING</p>
+</body>
+</html>
