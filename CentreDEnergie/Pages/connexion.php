f8411724d8de4c0c3555479c07aa92655678b697
bojan97 <b2thes@hotmail.com>
2017-01-17 16:12:11 -0500

    Add files via upload

---
+++ CentreDEnergie/Pages/connexion.php
@@ -0,0 +1,60 @@
+<?php
+
+include("headerLayout.php");
+
+?>
+
+
+<div class="row">
+	<div class="col-md-12"><h2>Connexion</h2></div>
+</div>
+<div class="row">
+
+	<div class="col-md-3"></div>
+	<div class="col-md-5">
+	<form method="POST">
+	<label for="username" id="lblUser" >Nom d'Utilisateur: </label>
+	<input type="text" name="username" class="form-control" id="username" required="required" maxlength="15" autofocus>
+	<br>
+	<label for="password">Mot de Passe: </label>
+	<input type="password" name="password" class="form-control"  id="password" required="required">
+	<br>
+	<input type="submit" class="btn btn-danger" value="Connexion" required="required">
+	</form>
+	</div>
+	<div class="col-md-4"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+
+include("footerLayout.php");
+
+
+?>
--- CentreDEnergie/Pages/salle.php
+++ CentreDEnergie/Pages/salle.php
@@ -9,7 +9,9 @@ include("headerLayout.php");
 <div class="post" style="padding-top:450px"><!-- THE PADDING HERE IS TEMPORARY -->
 	
 <hr id="tareaBorder">
-<textarea rows="3" placeholder="Écrivez votre message ici..."autofocus></textarea><input type="button" id="button" value="Envoyer">
+<textarea rows="3" placeholder="Écrivez votre message ici..."autofocus></textarea>
+<input type="button" id="button" class="input_Button" value="Envoyer">
+<button id="button" class="button_Button"><span class="glyphicon glyphicon-share-alt"></span></button>
 </div>
 
 <?php
