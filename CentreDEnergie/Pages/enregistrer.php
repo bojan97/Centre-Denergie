3ee0410764ce7b488f882856249641aa5df2a6c2
bojan97 <b2thes@hotmail.com>
2017-01-19 16:06:48 -0500

    Add files via upload

--- CentreDEnergie/Pages/enregistrer.php
+++ CentreDEnergie/Pages/enregistrer.php
@@ -8,42 +8,39 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 </script>
-<div class="row">
-	<div class="col-md-5"></div>
-	<div class="col-md-4"><h2>Enregistrement</h2></div>
-	<div class="col-md-3"></div>
+<div class="row" id="title_register">
+	<h2>Enregistrement</h2>
 </div>
 <div class="row">
-
-	<div class="col-md-3"></div>
-	<div class="col-md-5">
 	<form method="POST">
-	<label for="username" id="lblUser" >Nom d'Utilisateur: </label>
-	<input type="text" name="username" class="form-control" id="username" required="required" maxlength="15" autofocus><span id="usernameTip" class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="Le nom d'utilisateur peut avoir jusqu'à 15 charactères et doit être unique."></span>
-	<br>
-	<label for="password">Mot de Passe: </label>
-	<input type="password" name="password" class="form-control"  id="password" required="required"><span id="passwordTip" class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="insert password requirements here"></span>
-	<br>
-	<label for="rpassword">Répétez Mot de Passe: </label>
-	<input type="password" name="rpassword" class="form-control"  id="rpassword" required="required">
-	<br>
-	<label for="fname">Prénom: </label>
-	<input type="text" name="fname" class="form-control"  id="fname" required="required"><span id="nameTip" class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="Votre nom ne sera pas visible publiquement"></span>
-	<br>
-	<label for="lname">Nom: </label>
-	<input type="text" name="lname" class="form-control"  id="lname" required="required">
-	<br>
-	<input type="submit" class="btn btn-danger" value="Enregistrer">
+		<div id="div_Username">
+			<label for="username" id="lblUser" >Nom d'Utilisateur: </label>
+			<input type="text" name="username" class="form-control" id="username" required="required" maxlength="15" autofocus>
+			<span id="usernameTip" class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="Le nom d'utilisateur peut avoir jusqu'à 15 charactères et doit être unique."></span>			
+		</div>
+		<br>
+		<div id="div_Password">
+			<label for="password">Mot de Passe: </label>
+			<input type="password" name="password" class="form-control"  id="password" required="required">
+			<span id="passwordTip" class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="insert password requirements here"></span>
+		</div>
+		<br>
+		<label for="rpassword">Répétez Mot de Passe: </label>
+		<input type="password" name="rpassword" class="form-control"  id="rpassword" required="required">
+		<br>
+		<div id="div_Name">
+			<label for="fname">Prénom: </label>
+			<input type="text" name="fname" class="form-control"  id="fname" required="required">
+			<span id="nameTip" class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="Votre nom ne sera pas visible publiquement"></span>
+		</div>
+		<br>
+		<label for="lname">Nom: </label>
+		<input type="text" name="lname" class="form-control"  id="lname" required="required">
+		<br>
+		<input type="submit" class="btn btn-danger" value="Enregistrer">
 	</form>
-	</div>
-	<div class="col-md-4"></div>
 </div>
-
-
 
 <?php
-
 include("footerLayout.php");
-
-
 ?>
--- CentreDEnergie/Pages/techniques.php
+++ CentreDEnergie/Pages/techniques.php
@@ -4,8 +4,110 @@ include("headerLayout.php");
 
 ?>
 
-
-
+<div class="row">
+	<div class="col-md-12"><h2>Mes Techniques</h2></div>
+</div>
+<table class="tg">
+  <tr>
+    <th class="tg-amwm">Technique</th>
+    <th class="tg-amwm">Blanche</th>
+    <th class="tg-amwm">Jaune</th>
+    <th class="tg-amwm">Orange</th>
+    <th class="tg-amwm">Violette</th>
+    <th class="tg-amwm">Bleue</th>
+    <th class="tg-amwm">Verte</th>
+    <th class="tg-amwm">Brune</th>
+    <th class="tg-amwm">Noire</th>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups de Poing</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups de Pied</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">5</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Bloques</td>
+    <td class="tg-baqh">8</td>
+    <td class="tg-baqh">8</td>
+    <td class="tg-baqh">8</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups de Genou</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">1</td>
+    <td class="tg-baqh">1</td>
+    <td class="tg-baqh">1</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">1</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups d'Épaule</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Combinaisons</td>
+    <td class="tg-baqh">3,6,7</td>
+    <td class="tg-baqh">12,18</td>
+    <td class="tg-baqh">2,3</td>
+    <td class="tg-baqh">8,9</td>
+    <td class="tg-baqh">10,15,16</td>
+    <td class="tg-baqh">11,13,14</td>
+    <td class="tg-baqh">1,21,19,26</td>
+    <td class="tg-baqh">22</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Kempos</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">6</td>
+    <td class="tg-baqh">10</td>
+    <td class="tg-baqh">12</td>
+    <td class="tg-baqh">5</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Formes</td>
+    <td class="tg-baqh">Pinan 1</td>
+    <td class="tg-baqh">Pinan 2</td>
+    <td class="tg-baqh">Kata 1</td>
+    <td class="tg-baqh">Kata 2</td>
+    <td class="tg-baqh">Pinan 3, Pinan 4, Forme de Bloques</td>
+    <td class="tg-baqh">Pinan 5, Forme de la Grue</td>
+    <td class="tg-baqh">Kata 3, Kata 4, Kata 5, Forme du Prunier, Two Man Fist Set A</td>
+    <td class="tg-baqh">Kata 6, Ansuki, Two Man Fist Set B</td>
+  </tr>
+</table>
 
 
 <?php
