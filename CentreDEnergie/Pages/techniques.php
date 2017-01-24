ce9731588e4b8f4799dbfd6c5389a4057a468d97
bojan97 <b2thes@hotmail.com>
2017-01-24 16:59:01 -0500

    Add files via upload

--- CentreDEnergie/Pages/techniques.php
+++ CentreDEnergie/Pages/techniques.php
@@ -1,15 +1,114 @@
 <?php
-
 include("headerLayout.php");
-
 ?>
 
-
-
-
+<div class="row">
+	<div class="col-md-12"><h2>Mes Techniques</h2></div>
+</div>
+<div id="table">
+<table class="tg">
+  <tr>
+    <th class="tg-amwm">Techniques</th>
+    <th class="tg-amwm"><font color="white">Blanche</font></th>
+    <th class="tg-amwm"><font color="#ddd840">Jaune</font></th>
+    <th class="tg-amwm"><font color="orange">Orange</font></th>
+    <th class="tg-amwm"><font color="#d365cf">Violette</font></th>
+    <th class="tg-amwm"><font color="#4286f4">Bleue</font></th>
+    <th class="tg-amwm"><font color="green">Verte</font></th>
+    <th class="tg-amwm"><font color="#93503c">Brune</font></th>
+    <th class="tg-amwm"><font color="black">Noire</font></th>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups de Poing</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">7</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups de Pied</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">5</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Bloques</td>
+    <td class="tg-baqh">8</td>
+    <td class="tg-baqh">8</td>
+    <td class="tg-baqh">8</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups de Genou</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">1</td>
+    <td class="tg-baqh">1</td>
+    <td class="tg-baqh">1</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">1</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Coups d'Épaule</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">7</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">0</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm" style="font-size:11px">Combinaisons</td>
+    <td class="tg-baqh">3,6,7</td>
+    <td class="tg-baqh">12,18</td>
+    <td class="tg-baqh">2,3</td>
+    <td class="tg-baqh">8,9</td>
+    <td class="tg-baqh">10,15,16</td>
+    <td class="tg-baqh">11,13,14</td>
+    <td class="tg-baqh">1,21,19,26</td>
+    <td class="tg-baqh">22</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Kempos</td>
+    <td class="tg-baqh">0</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">3</td>
+    <td class="tg-baqh">4</td>
+    <td class="tg-baqh">6</td>
+    <td class="tg-baqh">10</td>
+    <td class="tg-baqh">12</td>
+    <td class="tg-baqh">5</td>
+  </tr>
+  <tr>
+    <td class="tg-amwm">Formes</td>
+    <td class="tg-baqh">Pinan 1</td>
+    <td class="tg-baqh">Pinan 2</td>
+    <td class="tg-baqh">Kata 1</td>
+    <td class="tg-baqh">Kata 2</td>
+    <td class="tg-baqh">Pinan 3, Pinan 4, Forme de Bloques</td>
+    <td class="tg-baqh">Pinan 5, Forme de la Grue</td>
+    <td class="tg-baqh">Kata 3, Kata 4, Kata 5, Forme du Prunier, Two Man Fist Set A</td>
+    <td class="tg-baqh">Kata 6, Ansuki, Two Man Fist Set B</td>
+  </tr>
+</table>
+</div>
 
 <?php
-
 include("footerLayout.php");
-
 ?>
