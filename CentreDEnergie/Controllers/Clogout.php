03d01ce0a1ae2b323b941a12675e3bc7234a57e7
zaad97 <saad_ahmed1997@hotmail.com>
2017-02-09 16:06:04 -0500

    Add files via upload

--- CentreDEnergie/Controllers/Clogout.php
+++ CentreDEnergie/Controllers/Clogout.php
@@ -3,6 +3,19 @@ session_start();
 
 $_SESSION["loginStatus"]=null;
 $_SESSION["student"]=null;
+
+if(isset($_SESSION["errorUsername"]))	$_SESSION["errorUsername"]=null;
+if (isset($_SESSION["errorPassword"]))	$_SESSION["errorPassword"]=null;
+if (isset($_SESSION["errorRpassword"]))	$_SESSION["errorRpassword"]=null;
+
+if(isset($_SESSION["username"]))	$_SESSION["username"]=null;
+if(isset($_SESSION["fname"]))	$_SESSION["fname"]=null;
+if(isset($_SESSION["lname"]))	$_SESSION["lname"]=null;
+
+if(isset($_SESSION["errorName"]))	$_SESSION["errorName"]=null;
+if(isset($_SESSION["error"]))	$_SESSION["error"]=null;
+
+
 setcookie("rememberStudent", "", time() - 3600, "/");
 setcookie("rememberTeacher", "", time() - 3600, "/");
 header("Location:/CentreDEnergie/Pages/connexion.php");
